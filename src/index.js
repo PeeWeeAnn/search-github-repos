886a4d1a020fb726726bded9757164db8ac7677e
Anna Kautts <annakautts@gmail.com>
2023-04-07 03:28:29 +0600

    validation input

--- src/index.js
+++ src/index.js
@@ -1,86 +1,110 @@
-import "./scss/app.scss";
+import "./scss/app.scss"
+
+let input = document.querySelector("input")
+let list = document.querySelector(".list")
 
 async function fetchRepos(text) {
 	let result = await fetch(
 		`https://api.github.com/search/repositories?q=${text}&page=1&per_page=10`,
 	)
 		.then((res) => res.json())
-		.catch((error) => console.log(error));
-	appendResults(result.items);
+		.catch((error) => console.log(error))
+
+	if (result.items.length) {
+		let searchMessageFound = document.createElement("p")
+
+		appendMessage(
+			list,
+			searchMessageFound,
+			"search_message",
+			`Найдены результаты по запросу ${text}`,
+		)
+
+		appendResults(result.items)
+	} else {
+		let searchMessageNotFound = document.createElement("p")
+
+		appendMessage(
+			list,
+			searchMessageNotFound,
+			"search_message",
+			`Ничего не найдено по запросу ${text}!`,
+		)
+	}
 }
 
 function appendResults(arrayOfResults) {
-	let resultsList = document.getElementById("results_list");
+	let resultsList = document.getElementById("results_list")
 
 	arrayOfResults.forEach((item) => {
-		let li = document.createElement("li");
-		let anchor = document.createElement("anchor");
-		let description = document.createElement("p");
-		let count = document.createElement("p");
-		let creatingData = document.createElement("p");
+		let li = document.createElement("li")
+		let anchor = document.createElement("anchor")
+		let description = document.createElement("p")
+		let count = document.createElement("p")
+		let creatingData = document.createElement("p")
 
-		anchor.textContent = item.name;
-		anchor.href = item.html_url;
-		anchor.target = "_blank";
+		anchor.textContent = item.name
+		anchor.href = item.html_url
+		anchor.target = "_blank"
 
-		description.textContent = item.description || "No description";
+		description.textContent = item.description || "No description"
 
-		count.textContent = "☆" + item.stargazers_count;
+		count.textContent = "☆" + item.stargazers_count
 
-		creatingData.textContent = item.created_at;
+		creatingData.textContent = item.created_at
 
-		li.append(anchor);
-		li.append(description);
-		li.append(count);
-		li.append(creatingData);
-		resultsList.append(li);
-	});
+		li.append(anchor)
+		li.append(description)
+		li.append(count)
+		li.append(creatingData)
+		resultsList.append(li)
+	})
 }
 
-let input = document.querySelector("input");
+function handleEnter() {
+	input.addEventListener("keydown", (event) => {
+		if (event.key === "Enter") {
+			event.preventDefault()
 
-input.addEventListener("keydown", (event) => {
-	if (event.key === "Enter") {
-		event.preventDefault();
+			if (document.querySelector(".search_message")) {
+				removeElement(document.querySelector(".search_message"))
+			}
 
-		if (document.querySelector(".search_message")) {
-			removeMessage(document.querySelector(".search_message"));
+			if (!event.currentTarget.value) {
+				let searchMessage = document.createElement("p")
+
+				appendMessage(
+					list,
+					searchMessage,
+					"search_message",
+					"Вы ничего не ввели в поисковую строку!",
+				)
+			}
+
+			fetchRepos(input.value)
+			input.value = ""
+			removeResults()
 		}
-
-		if (event.currentTarget.value) {
-			let list = document.querySelector(".list");
-
-			let searchMessage = document.createElement("p");
-
-			searchMessage.classList.add("search_message");
-			searchMessage.textContent = `Найдены результаты по запросу ${event.currentTarget.value}:`;
-
-			list.prepend(searchMessage);
-		} else {
-			let list = document.querySelector(".list");
-
-			let searchMessage = document.createElement("p");
-
-			searchMessage.classList.add("search_message");
-			searchMessage.textContent = "Вы ничего не ввели в поисковую строку!";
-
-			list.prepend(searchMessage);
-		}
-
-		fetchRepos(input.value);
-		input.value = "";
-		removeResults();
-	}
-});
+	})
+}
 
 function removeResults() {
-	let resultsList = document.querySelectorAll("#results_list > *");
+	let resultsList = document.querySelectorAll("#results_list > *")
 
 	resultsList.forEach((listItem) => {
-		listItem.remove();
-	});
+		removeElement(listItem)
+	})
 }
 
-function removeMessage(message) {
-	message.remove();
+function appendMessage(list, elementName, className, message) {
+	elementName.classList.add(className)
+	elementName.textContent = message
+
+	list.prepend(elementName)
 }
+
+function removeElement(message) {
+	message.remove()
+}
+
+handleEnter()
