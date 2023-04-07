bc16230131da86a8875eb94da0a1779cb4da1de5
Anna Kautts <annakautts@gmail.com>
2023-04-08 00:05:13 +0600

    validation, handlers, functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./scss/app.scss"
 
-let input = document.querySelector("input")
-let list = document.querySelector(".list")
+const search = document.querySelector(".search")
+const input = document.querySelector("input")
+const button = document.querySelector("button")
 
+// REQUEST DATA
 async function fetchRepos(text) {
 	let result = await fetch(
 		`https://api.github.com/search/repositories?q=${text}&page=1&per_page=10`,
@@ -11,34 +13,47 @@ async function fetchRepos(text) {
 		.catch((error) => console.log(error))
 
 	if (result.items.length) {
-		let searchMessageFound = document.createElement("p")
+		let foundMessage = document.createElement("p")
 
-		appendMessage(
-			list,
-			searchMessageFound,
+		appendElement(
+			search,
+			foundMessage,
 			"search_message",
-			`Найдены результаты по запросу ${text}`,
+			"Найдены результаты по запросу: ",
 		)
+
+		let span = document.createElement("span")
+
+		appendElement(foundMessage, span, "span", text)
+
+		fillEntry()
 
 		appendResults(result.items)
 	} else {
-		let searchMessageNotFound = document.createElement("p")
+		let notFoundMessage = document.createElement("p")
 
-		appendMessage(
-			list,
-			searchMessageNotFound,
+		appendElement(
+			search,
+			notFoundMessage,
 			"search_message",
-			`Ничего не найдено по запросу ${text}!`,
+			"Ничего не найдено по запросу: ",
 		)
+
+		let span = document.createElement("span")
+
+		appendElement(notFoundMessage, span, "span", text)
+
+		fillEntry()
 	}
 }
 
+// ACTIONS
 function appendResults(arrayOfResults) {
 	let resultsList = document.getElementById("results_list")
 
 	arrayOfResults.forEach((item) => {
 		let li = document.createElement("li")
-		let anchor = document.createElement("anchor")
+		let anchor = document.createElement("a")
 		let description = document.createElement("p")
 		let count = document.createElement("p")
 		let creatingData = document.createElement("p")
@@ -51,7 +66,7 @@ function appendResults(arrayOfResults) {
 
 		count.textContent = "☆" + item.stargazers_count
 
-		creatingData.textContent = item.created_at
+		creatingData.textContent = new Date(item.created_at).toLocaleDateString()
 
 		li.append(anchor)
 		li.append(description)
@@ -61,7 +76,35 @@ function appendResults(arrayOfResults) {
 	})
 }
 
-function handleEnter() {
+const handleClick = () => {
+	button.addEventListener("click", (event) => {
+		event.preventDefault()
+
+		if (document.querySelector(".search_message")) {
+			removeElement(document.querySelector(".search_message"))
+		}
+
+		if (!input.value.trim()) {
+			let searchMessage = document.createElement("p")
+
+			appendElement(
+				search,
+				searchMessage,
+				"search_message",
+				"Вы ничего не ввели в поисковую строку!",
+			)
+		} else if (input.value.length < 3) {
+			alert("Вы ввели менее 3-х символов.")
+			return false
+		}
+
+		fetchRepos(input.value)
+		input.value = ""
+		removeResults()
+	})
+}
+
+const handleEnter = () => {
 	input.addEventListener("keydown", (event) => {
 		if (event.key === "Enter") {
 			event.preventDefault()
@@ -70,15 +113,18 @@ function handleEnter() {
 				removeElement(document.querySelector(".search_message"))
 			}
 
-			if (!event.currentTarget.value) {
+			if (!event.currentTarget.value.trim()) {
 				let searchMessage = document.createElement("p")
 
-				appendMessage(
-					list,
+				appendElement(
+					search,
 					searchMessage,
 					"search_message",
 					"Вы ничего не ввели в поисковую строку!",
 				)
+			} else if (event.currentTarget.value.length < 3) {
+				alert("Вы ввели менее 3-х символов.")
+				return false
 			}
 
 			fetchRepos(input.value)
@@ -88,7 +134,7 @@ function handleEnter() {
 	})
 }
 
-function removeResults() {
+const removeResults = () => {
 	let resultsList = document.querySelectorAll("#results_list > *")
 
 	resultsList.forEach((listItem) => {
@@ -96,15 +142,28 @@ function removeResults() {
 	})
 }
 
-function appendMessage(list, elementName, className, message) {
-	elementName.classList.add(className)
-	elementName.textContent = message
+const fillEntry = () => {
+	let entry = document.querySelector(".search_message .span")
 
-	list.prepend(elementName)
+	if (entry) {
+		entry.addEventListener("click", (event) => {
+			input.value = event.target.textContent
+		})
+	}
+}
+
+// HELPERS
+function appendElement(parentNode, elementName, className, textContent) {
+	elementName.classList.add(className)
+	elementName.textContent = textContent
+
+	parentNode.append(elementName)
 }
 
 function removeElement(message) {
 	message.remove()
 }
 
+// EVENT LISTENERS
 handleEnter()
+handleClick()
